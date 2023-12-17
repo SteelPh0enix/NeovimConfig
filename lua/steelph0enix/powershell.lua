b76723a491f792969f34f3d0d9ac16c816962e23
SteelPh0enix <wojciech_olech@hotmail.com>
2023-12-17 21:40:24 +0100

    Fixed powershell config

--- lua/steelph0enix/powershell.lua
+++ lua/steelph0enix/powershell.lua
@@ -1,4 +1,4 @@
-vim.o.shell = 'C:\\Program Files\\PowerShell\\7\\pwsh.exe'
+vim.o.shell = "pwsh.exe"
 vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
 vim.o.shellredir = '2>&1 | %% { "$_" } | Out-File %s; exit $LastExitCode'
 vim.o.shellpipe = '2>&1 | %% { "$_" } | tee %s; exit $LastExitCode'
