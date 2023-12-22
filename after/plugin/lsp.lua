ff546f19ba01532e914ceebe17f9372e4899ff3e
SteelPh0enix <wojciech_olech@hotmail.com>
2023-12-22 09:54:58 +0100

    Modified Python LSP config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,14 +20,28 @@ lspconfig.pylsp.setup {
     settings = {
         pylsp = {
             plugins = {
-                pycodestyle = {
-                    maxLineLength = 100,
-                },
+                -- we use ruff and mypy only
+                black = { enabled = false },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
+                pylint = { enabled = false },
+                pyflakes = { enabled = false },
+                pycodestyle = { enabled = false },
                 ruff = {
                     enabled = true,
                     select = { 'F', 'E', 'W', 'C90', 'I', 'N', 'UP', 'ANN', 'FBT', 'B', 'A', 'C4', 'DTZ', 'FA', 'ISC', 'PIE', 'RET', 'SLF', 'SIM', 'TCH', 'PTH', 'PL', 'PERF', 'FLY' },
                     format = { 'F', 'E', 'W', 'I' },
                     lineLength = 100,
+                },
+                pylsp_mypy = {
+                    enabled = true,
+                    live_mode = true,
+                    strict = true,
+                },
+                -- also jedi for autocompletion i guess
+                jedi_completion = {
+                    enabled = true,
+                    fuzzy = true,
                 }
             },
         },
