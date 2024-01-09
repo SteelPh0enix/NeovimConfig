4e7415c433012bcee2cebca7d044236e89243521
SteelPh0enix <wojciech_olech@hotmail.com>
2024-01-09 15:16:18 +0100

    Added hard-coded Python paths for Windows

--- lua/steelph0enix/init.lua
+++ lua/steelph0enix/init.lua
@@ -2,6 +2,7 @@ if vim.loop.os_uname().sysname == "Windows_NT" then
     require("steelph0enix.powershell")
 end
 
+require("steelph0enix.python")
 require("steelph0enix.options")
 require("steelph0enix.remap")
 require("steelph0enix.packer")
---
+++ lua/steelph0enix/python.lua
@@ -0,0 +1,3 @@
+if vim.loop.os_uname().sysname == "Windows_NT" then
+    vim.g.python3_host_prog = "C:\\Users\\phoen\\AppData\\Local\\Programs\\Python\\Python312\\python.exe"
+end
