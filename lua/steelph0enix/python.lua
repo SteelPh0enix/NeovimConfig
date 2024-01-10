46eeba7da9868af294e7eb60c120fdd979052ac5
Wojciech Olech <wojciech.olech@n7space.com>
2024-01-10 10:38:50 +0100

    Hard-coded Linux path for python exec

--- lua/steelph0enix/python.lua
+++ lua/steelph0enix/python.lua
@@ -1,3 +1,6 @@
 if vim.loop.os_uname().sysname == "Windows_NT" then
     vim.g.python3_host_prog = "C:\\Users\\phoen\\AppData\\Local\\Programs\\Python\\Python312\\python.exe"
+else
+    vim.g.python3_host_prog = "/usr/bin/python3"
 end
+
