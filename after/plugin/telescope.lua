92b11037f97802fca5d6b0c96592e6c8830c7b90
SteelPh0enix <wojciech_olech@hotmail.com>
2024-01-30 13:35:48 +0100

    Added "find in all" shortcut for Telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local telescope = require('telescope.builtin')
 vim.keymap.set('n', '<C-f>f', telescope.find_files)
+vim.keymap.set('n', '<C-f>a', function() telescope.find_files({ hidden = true }) end)
 vim.keymap.set('n', '<C-f>p', telescope.git_files)
 vim.keymap.set('n', '<C-f>w', telescope.grep_string)
 vim.keymap.set('n', '<C-f>g', telescope.live_grep)
