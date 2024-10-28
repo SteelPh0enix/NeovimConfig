4cbb0f4f488e19a490ae88f81dadb30e3bb181d7
SteelPh0enix <wojciech_olech@hotmail.com>
2024-10-28 13:13:38 +0100

    Disabled DAP for now

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -22,60 +22,41 @@ vim.keymap.set('n', '<Leader>ds', function()
     widgets.centered_float(widgets.scopes)
 end)
 
--- i'm using vscode-cpptools for C and C++. I liked it in VSCode, i guess i should like it in here.
--- Download the latest release from https://github.com/microsoft/vscode-cpptools/releases
--- and unzip it to proper directory.
-
-if vim.loop.os_uname().sysname == "Windows_NT" then
-    dap.adapters.cppdbg = {
-        id = 'cppdbg',
-        type = 'executable',
-        command = 'C:\\Users\\phoen\\cpptools\\extension\\debugAdapters\\bin\\OpenDebugAD7.exe',
-        options = {
-            detached = false,
-        }
-    }
-else -- if not on windows, must be on linux
-    dap.adapters.cppdbg = {
-        id = 'cppdbg',
-        type = 'executable',
-        command = '/home/steelph0enix/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
-    }
-end
+-- TODO update and fix
 
 -- more docs: https://github.com/mfussenegger/nvim-dap/wiki/C-C---Rust-(gdb-via--vscode-cpptools)
-dap.configurations.cpp = {
-    {
-        name = 'Debug file',
-        type = 'cppdbg',
-        request = 'launch',
-        program = function()
-            return vim.fn.input('Path to exec: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}',
-        stopAtEntry = true,
-    },
-}
-
-dap.configurations.c = dap.configurations.cpp
-
--- Python DAP config
-local python_dap = require('dap-python')
-
-python_dap.setup(vim.g.python3_host_prog)
-python_dap.test_runner = 'pytest'
-
-dap.configurations.python = {
-    {
-        name = 'Debug current file in project',
-        type = 'python',
-        request = 'launch',
-        program = '${file}',
-        env = {
-            PYTHONPATH = '${workspaceFolder}',
-        }
-    },
-}
-
-vim.keymap.set('n', '<Leader>dm', python_dap.test_method)
-vim.keymap.set('n', '<Leader>dc', python_dap.test_class)
+-- dap.configurations.cpp = {
+--     {
+--         name = 'Debug file',
+--         type = 'cppdbg',
+--         request = 'launch',
+--         program = function()
+--             return vim.fn.input('Path to exec: ', vim.fn.getcwd() .. '/', 'file')
+--         end,
+--         cwd = '${workspaceFolder}',
+--         stopAtEntry = true,
+--     },
+-- }
+--
+-- dap.configurations.c = dap.configurations.cpp
+--
+-- -- Python DAP config
+-- local python_dap = require('dap-python')
+--
+-- python_dap.setup(vim.g.python3_host_prog)
+-- python_dap.test_runner = 'pytest'
+--
+-- dap.configurations.python = {
+--     {
+--         name = 'Debug current file in project',
+--         type = 'python',
+--         request = 'launch',
+--         program = '${file}',
+--         env = {
+--             PYTHONPATH = '${workspaceFolder}',
+--         }
+--     },
+-- }
+--
+-- vim.keymap.set('n', '<Leader>dm', python_dap.test_method)
+-- vim.keymap.set('n', '<Leader>dc', python_dap.test_class)
