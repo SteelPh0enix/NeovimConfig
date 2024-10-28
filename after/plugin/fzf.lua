5a3e84de3a2a8913d234ba19b129c1aed59a6151
Wojciech Olech <wojciech.olech@n7space.com>
2024-10-28 14:18:52 +0100

    Added fzf-lua keybinds

--- after/plugin/fzf.lua
+++ after/plugin/fzf.lua
@@ -1,3 +1,27 @@
 local fzf = require("fzf-lua")
 
 fzf.setup({})
+
+vim.keymap.set("n", "<c-F>b", fzf.buffers)
+vim.keymap.set("n", "<c-F>f", fzf.files)
+vim.keymap.set("n", "<c-F>q", fzf.quickfix)
+vim.keymap.set("n", "<c-F>g", fzf.live_grep)
+vim.keymap.set({ "n", "v" }, "<c-F>gc", fzf.grep_cword)
+vim.keymap.set("v", "<c-F>gv", fzf.grep_visual)
+
+vim.keymap.set("n", "<c-G>f", fzf.git_files)
+vim.keymap.set("n", "<c-G>s", fzf.git_status)
+
+vim.keymap.set("n", "<c-L>r", fzf.lsp_references)
+vim.keymap.set("n", "<c-L>df", fzf.lsp_definitions)
+vim.keymap.set("n", "<c-L>dc", fzf.lsp_declarations)
+vim.keymap.set("n", "<c-L>i", fzf.lsp_implementations)
+vim.keymap.set("n", "<c-L>l", fzf.lsp_finder)
+vim.keymap.set("n", "<c-L>c", fzf.lsp_code_actions)
+vim.keymap.set("n", "<c-L>dd", fzf.diagnostics_document)
+vim.keymap.set("n", "<c-L>dw", fzf.diagnostics_workspace)
+
+vim.keymap.set({ "n", "v", "i" }, "<c-F>cp", fzf.complete_path)
+vim.keymap.set({ "n", "v", "i" }, "<c-F>cf", fzf.complete_file)
+vim.keymap.set({ "n", "v", "i" }, "<c-F>cl", fzf.complete_line)
+vim.keymap.set({ "n", "v", "i" }, "<c-F>cb", fzf.complete_bline)
